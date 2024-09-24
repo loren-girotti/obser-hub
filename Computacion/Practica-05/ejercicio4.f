c5f5c1065e0668ebf7ae4f6538246bf39c355b1d
Lorenzo Girotti <lgirotti@carina.fcaglp.unlp.edu.ar>
2024-09-24 12:45:01 -0300

    falta terminar el ejercicio4.f

---
+++ Computacion/Practica-05/ejercicio4.f
@@ -0,0 +1,28 @@
+C este programa devuelve el número complejo de mayor módulo dentro de un
+C array de M números complejos.
+      program complex_mod
+      integer nmax,m,indice
+      parameter (nmax=100)
+      real a,b,z(nmax),max
+      complex C(nmax) !nmax es el valor maximo de componentes
+      read(*,*) m !m seran las componentes de trabajo
+      do i=1,m
+      write(*,*) "Indique las componentes del complejo",i
+      read(*,*) a,b
+      c(i)=complex(a,b)
+      write(*,*) "Registrado."
+
+      z(i)=abs(c(i))
+
+      end do
+      
+      max=0.0
+      
+      do i=1,m
+              if (z(i).gt.max) then
+                      max=z(i)
+                      indice=i
+              end if
+      end do 
+
+             end
