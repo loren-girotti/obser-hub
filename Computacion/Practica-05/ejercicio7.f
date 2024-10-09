9f295f6089e622588cd78c88dea064fdc4eb8c9b
Lorenzo Girotti <lorenzogirotti00@gmail.com>
2024-10-08 23:13:02 -0300

    ejercicio7 bien hecho

--- Computacion/Practica-05/ejercicio7.f
+++ Computacion/Practica-05/ejercicio7.f
@@ -3,29 +3,34 @@ C componentes de los vectores que se hallan en PO5-Vectores.dat
 C en orden descendente.
 C #############################################################
       program bubble_sort
-      integer nmax, N(30), i, m
-      parameter (nmax=30)
-      real v(nmax)
+      logical desordenado
+      integer n, j, i, IO
+      real v(100)
       open(50,file="P05-Vectores.dat")
 
       read(50,*)
-10    format(X,I2,3X,29(F6.2))
-      read(50,10) N(1), (V(i), i=1,30))
-      write(*,*) n(1), v(1), v(2)
+      IO=0
+      do while(IO.eq.0)
+10    format(X,I2,3X,100(F6.2,X))
+      read(50,10,IOSTAT=IO) n, (V(i), i=1,n)
 C Bubble Sort Algorithm in fortran77. ########
-C      do i=1,n-1
-C            do j=1,n-i
-C                  if(v(j).lt.v(j+1)) then
-C                  aux=v(j)
-C                  v(j)=v(j+1)
-C                  v(j+1)=aux 
-C                  endif
-C            enddo
-C      enddo
-CC ###########################################
-C      write(*,*)"el vector en orden descendente"
-C      do i=1,n
-C      write(*,*) v(i)
-C      enddo
-C
+      desordenado=.true.
+      i=0
+        do while(desordenado)
+        i=i+1
+        desordenado=.false.
+              do j=1,n-i
+                    if(v(j).lt.v(j+1)) then
+                    aux=v(j)
+                    v(j)=v(j+1)
+                    v(j+1)=aux 
+                    desordenado=.true.
+                    endif
+              enddo
+        enddo
+C #########################################################
+        write(*,*)"Las componentes del vector ordenado:"
+        write(*,'(100(F6.2,X))') (v(j), j=1,n)
+      enddo
+
       end
