6c154b4804ba7f0254080792712523be3a335f63
Lorenzo Girotti <lorenzogirotti00@gmail.com>
2024-11-09 21:20:46 -0300

    funcion factorial, preguntar

---
+++ Computacion/Practica-06/ejercicio5.f
@@ -0,0 +1,26 @@
+C PROGRAMA PARA HACER UNA FUNCION PARA CALCULAR n!
+
+      program factorial_func
+      implicit none
+      integer fact, n, r, Cnr
+      write(*,*) "Ingrese n, y r para calcualr C(n,r)"
+      read(*,*) n,r
+      write(*,*) "n es ",n," r es ",r
+
+      Cnr = fact(n)/(fact(r)*fact(n-r))
+10    format(A10,I25)
+      write(*,10) "C(n,r) = ",Cnr
+
+      end
+
+      integer function fact(n)
+      integer n, i, facto
+      facto=1
+      do i = 1, n
+            facto=i*facto
+      enddo
+      fact = facto
+      return
+      end
+
+      
