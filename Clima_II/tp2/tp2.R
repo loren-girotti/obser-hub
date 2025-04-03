048e5df3576b687c5ce2306ee026c41b1ca61ac7
Lorenzo Girotti <lorenzogirotti00@gmail.com>
2025-04-03 13:58:05 -0300

    100% tp2

--- Clima_II/tp2/tp2.R
+++ Clima_II/tp2/tp2.R
@@ -74,27 +74,28 @@ for(i in 1:nrow(mdq)){
 }
 
 #e)
-mdq |>
-  select("Estacion","Fecha","PP") |>
+mdq %>%
+  select("Estacion","Fecha","PP") %>%
   mutate(Logico=ifelse(mdq$PP>0,"TRUE","FALSE"))
 
 #-----------
 #EJERCICIO 3
-pp_pergamino<-read_tsv(file="./tp2/Pergamino_INTA.tsv",na ="-99.9")
+pp_pergamino<-read_tsv(file="./tp2/Pergamino_INTA.tsv",col_names=F,na ="-99.9")
+pp_pergamino<-pp_pergamino[,1:6]
 
 #a)
-names(pp_pergamino)<-c("Year","Month","Day","PP","Tmax","Tmin","Logico")
+names(pp_pergamino)<-c("Year","Month","Day","PP","Tmax","Tmin")
 
 #b)
-pp_pergamino<-unite(pp_pergamino,"Fecha","Year","Month","Day",sep = "-")|>
-  select("Fecha","PP","Tmax","Tmin")
+pp_pergamino<-unite(pp_pergamino,"Fecha","Year","Month","Day",sep = "-")
+# pp_pergamino<-unite(pp_pergamino,"Fecha","Year","Month","Day",sep = "-") %>% 
 
 pp_pergamino$Fecha<-ymd(pp_pergamino$Fecha)
 
 #c)
-pp_1996<-pp_pergamino |>
-  select("Fecha","PP") |>
-  filter(year(Fecha)==1996,PP>0) |>
+pp_1996<-pp_pergamino %>%
+  select("Fecha","PP") %>%
+  filter(year(Fecha)==1996,PP>0) %>%
   summarise(Acumulado_1996=sum(PP))
 
 #-------------
@@ -102,14 +103,13 @@ pp_1996<-pp_pergamino |>
 caudales<-read_csv("./tp2/df_Caudales.csv")
 
 #a)
-library(purrr)
 missing_values<- function(vector){
   nas_porcentual<-length(which(is.na(vector)))/length(vector)
   return(nas_porcentual)
 }
   
 #b)
-datos_faltantes<-select(.data=caudales,where(is.numeric)) |>
+datos_faltantes<-select(.data=caudales,-Fecha) %>%
   map(.f=missing_values)
 
 mas_datos<-which.min(datos_faltantes);names(mas_datos);datos_faltantes$PasodeIndios
@@ -118,17 +118,66 @@ mas_datos<-which.min(datos_faltantes);names(mas_datos);datos_faltantes$PasodeInd
 
 #c)
 caudales_t<-pivot_longer(data=caudales,
-                         cols=c("Andacollo","BajadaDelAgrio","DiqueBallester",
-                                "LosCarrizos","LosMaitenes","PasodeIndios","Varvarco"),
-                         names_to = "Estaciones",
+                         cols=-Fecha, #Selecciono todo menos la Fecha
+                         names_to = "Estacion",
                          values_to = "Caudal")
 
-caudales_t<-sort
+
 
 #d)
+caudal_anual_PasoIndios<-filter(.data=caudales_t,Estacion=="PasodeIndios")%>%
+  mutate(Año=year(Fecha))%>%
+  group_by(Año)%>%
+  summarise(Caudal_Medio=mean(Caudal,na.rm=T))
 
+#e)
+ggplot(data=caudal_anual_PasoIndios,aes(x=Año,y=Caudal_Medio))+
+  geom_point()+
+  geom_line(col="salmon")+
+  ylab("Caudal medio")+
+  xlab("Año")+
+  ggtitle("Caudal Medio Anual - Paso de Indios")
+
+#----
+# EJERCICIO 5
+
+#a)
+caudales_comahue<-read_csv("./tp2/CaudalMedioDiario_Comahue.csv")
+
+Qsep_Andacollo<-caudales_comahue%>%
+  filter(Estacion=="Andacollo",month(Fecha)==9)
+
+ggplot(data=Qsep_Andacollo,aes(x=Caudal))+
+  geom_histogram(col="blue",fill="blue")+
+  scale_x_continuous(breaks=seq(0,1000,by=100))+
+  scale_y_continuous(breaks=seq(0,400,by=50))
+
+#b)
+Qmensual_Andacollo<-caudales_comahue%>%
+  filter(Estacion=="Andacollo")%>%
+  mutate(Mes=month(Fecha))%>%
+  group_by(Mes)%>%
+  summarise(ValorMedio=mean(Caudal,na.rm=T))
+
+ggplot(data=Qmensual_Andacollo,aes(x=factor(Mes),y=ValorMedio))+
+  geom_bar(stat="identity",col="black",fill="salmon")+
+  xlab("Mes")+
+  ylab("Caudal Medio")+
+  ggtitle("Caudal Medio Mensual - Andacollo")
+
+#c)
+Qmensual_anual_Andacollo<-caudales_comahue%>%
+  filter(Estacion=="Andacollo")%>%
+  mutate(Año=year(Fecha),Mes=month(Fecha))%>%
+  group_by(Año,Mes)%>%
+  summarise(ValorMedio=mean(Caudal,na.rm=T))
                          
-
+ggplot(data=Qmensual_anual_Andacollo,aes(x=factor(Mes),y=ValorMedio))+
+  geom_boxplot()+
+  scale_y_continuous(breaks=seq(0,400,by=50))+
+  xlab("Mes")+
+  ylab("Caudal Medio")+
+  ggtitle("Caudal Medio Anual por Mes - Andacollo")
 
 
 
